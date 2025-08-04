58a5151e213646e5b6ce588fb94a9ce23697790d
MultiverseChronicles <multiversechroniclesonline@gmail.com>
2025-08-05 00:09:48 +0800

    Update gen.go

--- net/gen.go
+++ net/gen.go
@@ -7,10 +7,10 @@ import (
 	"sync"
 	"testing"
 
-	tetc "github.com/libp2p/go-libp2p-testing/etc"
-	ci "github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/test"
+	tetc "github.com/MultiverseChronicles/go-libp2p-testing/etc"
+	ci "github.com/MultiverseChronicles/go-libp2p/core/crypto"
+	"github.com/MultiverseChronicles/go-libp2p/core/peer"
+	"github.com/MultiverseChronicles/go-libp2p/core/test"
 
 	ma "github.com/multiformats/go-multiaddr"
 )
